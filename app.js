23e95deec0343707017d04232342ea4b8b3fdfa4
Valeria Riera <ingvaleriariera@gmail.com>
2026-04-14 17:28:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// app.js
+const express = require('express');
+const os = require('os');
 
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Hello from ${os.hostname()}!`);
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
